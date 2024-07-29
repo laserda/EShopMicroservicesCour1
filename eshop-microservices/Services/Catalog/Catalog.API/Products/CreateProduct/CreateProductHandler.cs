f576ff462afb0a85ff584231c21688cb6e261253
koman <komanatsefabriceigor@gmail.com>
2024-07-29 06:36:21 -0400

    Init projet

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommanHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        session.Store(product);
+        await session.SaveChangesAsync();
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
